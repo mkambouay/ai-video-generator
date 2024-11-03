e35e2e02e575511f2ae582a33eb9246828f5be89
= <mkambouay@gmail.com>
2024-11-03 15:51:27 +0100

    fix:Midleware bug + add google font

--- app/povider.js
+++ app/povider.js
@@ -1,5 +1,34 @@
-import React from "react";
+"use client";
+import { db } from "@/configs/db";
+import { Users } from "@/configs/schema";
+import { useUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import React, { useEffect } from "react";
 
 export default function Provider({ children }) {
+  //getting user information from the useUser hook from clerk
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && isNewUser();
+  }, [user]);
+
+  const isNewUser = async () => {
+    const result = await db
+      .select()
+      .from(Users)
+      .where(eq(Users.email, user?.primaryEmailAddress?.emailAddress));
+
+    console.log(result);
+
+    if (!result[0]) {
+      await db.insert(Users).values({
+        name: user.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        imageUrl: user?.imageUrl,
+      });
+    }
+  };
+
   return <div>{children}</div>;
 }
